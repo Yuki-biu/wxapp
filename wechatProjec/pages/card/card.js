ecbbc7682cdba1de8bac57feddc579d74c7ec007
Yuki-biu <yuki199908@163.com>
2020-07-18 17:57:05 +0800

    myCard

--- wechatProjec/pages/card/card.js
+++ wechatProjec/pages/card/card.js
@@ -5,6 +5,12 @@ Page({
    * 页面的初始数据
    */
   data: {
+    brandList:[
+      'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3623917947,1625969810&fm=26&gp=0.jpg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1595074827820&di=8c7d6a2ca0ad18ed55254cafcf9ea1f9&imgtype=0&src=http%3A%2F%2Fpic139.nipic.com%2Ffile%2F20170824%2F7299487_215405173031_2.jpg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1595074849512&di=b8ffe49e414d5d256595203e74378736&imgtype=0&src=http%3A%2F%2Fpic138.nipic.com%2Ffile%2F20170815%2F12006035_160859629036_2.jpg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1595074869545&di=9598f576c0395ebf61f7392c16832cce&imgtype=0&src=http%3A%2F%2Fimg4.99114.com%2Fgroup10%2FM00%2F47%2F4C%2FrBADs1optbeAEHTzAAIoaJQfFRY043.jpg'
+    ]
 
   },
 
