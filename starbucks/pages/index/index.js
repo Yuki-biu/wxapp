f2933721076fd7732df805b0d1385a7f1f79fd5e
Foxcc6 <1065318755@qq.com>
2020-07-25 20:52:45 +0800

    星巴克首页

--- starbucks/pages/index/index.js
+++ starbucks/pages/index/index.js
@@ -4,10 +4,133 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    imgUrls:[
+        '/img/banner1.png',
+        '/img/banner2.png',
+        '/img/banner3.png'
+    ] ,
+    category:[
+      {name:'全部', id:'quanbu'},
+      {name:'夏日', id:'xiari'},
+      {name:'感谢', id:'ganxie'},
+      {name:'生日', id:'shengri'},
+      {name:'加油', id:'jiayou'},
+      {name:'挚爱', id:'zhiai'},
+      {name:'萌系', id:'mengxi'},
+      {name:'咖啡', id:'kafei'},
+    ],
+    detail:
+      [
+        {
+          id:'quanbu',
+          banner:'../../img/pcd1.png',
+          desc:'人鱼控  星礼卡',
+          money:'￥100',
+          icon:'../../img/preicon.png'     
+        },
+        {
+          id:'quanbu',
+          banner:'../../img/pcd2.png',
+          desc:'拿铁  电子礼品卷一张',
+          money:'￥32',
+          icon:'../../img/preicon.png'     
+        },
+        {
+          id:'quanbu',
+          banner:'../../img/pcd3.png',
+          desc:'百香果桃桃茶云乌龙  电子饮品劵1张',
+          money:'￥38',
+          icon:'../../img/preicon.png'     
+        },
+        {
+          id:'quanbu',
+          banner:'../../img/pcd4.png',
+          desc:'你是我的小淘气  星礼卡',
+          money:'￥50',
+          icon:'../../img/preicon.png'     
+        },
+        {
+          id:'quanbu',
+          banner:'../../img/pcd5.png',
+          desc:'Highfive  星礼卡',
+          money:'￥199',
+          icon:'../../img/preicon.png'     
+        },
+        {
+          id:'quanbu',
+          banner:'../../img/pcd6.png',
+          desc:'焦糖玛奇朵  电子饮品券1张',
+          money:'￥36',
+          icon:'../../img/preicon.png'     
+        },
+        {
+          id:'quanbu',
+          banner:'../../img/pcd7.png',
+          desc:'火龙果番石榴茶云乌龙  电子饮品券一张',
+          money:'￥38',
+          icon:'../../img/preicon.png'     
+        },
+        {
+          id:'quanbu',
+          banner:'../../img/pcd8.png',
+          desc:'生日快乐  星礼卡',
+          money:'￥100',
+          icon:'../../img/preicon.png'     
+        },
+        {
+          id:'quanbu',
+          banner:'../../img/pcd9.png',
+          desc:'干杯  星礼卡',
+          money:'￥288',
+          icon:'../../img/preicon.png'     
+        },
+        {
+          id:'quanbu',
+          banner:'../../img/pcd10.png',
+          desc:'拿铁  电子饮品券5张',
+          money:'￥160',
+          icon:'../../img/preicon.png'     
+        },
+        {
+          id:'quanbu',
+          banner:'../../img/pcd11.png',
+          desc:'THANK YOU  星礼卡',
+          money:'￥100',
+          icon:'../../img/preicon.png'     
+        },
+        {
+          id:'quanbu',
+          banner:'../../img/pcd12.png',
+          desc:'听你哒  星礼卡',
+          money:'￥199',
+          icon:'../../img/preicon.png'     
+        },
+      ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   },
   //事件处理函数
   bindViewTap: function() {
